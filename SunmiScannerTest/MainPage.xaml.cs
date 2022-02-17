600e2c1c275c31cd79b46d8f91faafe332da6513
Eoghan Bradshaw <eoghan.bradshaw@gmail.com>
2022-02-17 15:46:17 +0000

    Initial commit

---
+++ SunmiScannerTest/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace SunmiScannerTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            KeyPressed += CustomPage_KeyPressed;
+
+        }
+
+        public event EventHandler<KeyEventArgs> KeyPressed;
+
+        public void SendKeyPressed(object sender, KeyEventArgs e)
+        {
+            KeyPressed?.Invoke(sender, e);
+        }
+
+        private void CustomPage_KeyPressed(object sender, KeyEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Key pressed: " + e.Key);
+        }
+    }
+
+    public class KeyEventArgs : EventArgs
+    {
+        public string Key { get; set; }
+    }
+}
+
